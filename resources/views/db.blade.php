103a72d92639116bca63d89032b97ce00d84618d
meshal <mmmm3122@gmail.com>
2022-03-19 19:19:23 +0300

    dlt

--- resources/views/db.blade.php
+++
@@ -1,7 +0,0 @@
-<h1>Add mem to db</h1>
-<form action="add" method="post">
-    @csrf
-    <input type="text" name="name" placeholder="name"> <br> <br>
-    <input type="text" name="email" placeholder="email"> <br> <br>
-    <button type="submit">Add</button>
-</form>
